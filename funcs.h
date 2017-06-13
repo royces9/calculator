1b4d24dfe50964782eb707da5b8fbaef2f7d441a
royces9 <rrysato@gmail.com>
2017-06-12 18:23:01 -0700

    Add files via upload

---
+++ funcs.h
@@ -0,0 +1,18 @@
+#ifndef FUNCS
+#define FUNCS
+
+typedef struct{
+  char name[256][256];
+  double value[256];
+  int occ;
+  int count;
+} vari;
+
+double op(double a, double b, char o);
+double ops(double a, char o);
+void exec_num(stint* num, char ch);
+int funcfind(char buffer[]);
+int charfind(char buffer[], stint* num, stchar* ch, double ans, vari* var, int* tok);
+int varcheck(vari* list, char inp[]);
+
+#endif //FUNCS
