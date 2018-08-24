e2a8606ea3821753f9be1b1ae3e28f713fb59644
Royce <rrysato@gmail.com>
2018-08-23 17:42:32 -0700

    userFunctions.c: changed order of checkFunctionName userFunctions.h: added checkFunctionName

--- userFunctions.h
+++ userFunctions.h
@@ -5,5 +5,6 @@ matrix *findUserFunction(char *functionName, char **functionArgs, vari *var, err
 char *checkConfig(char *functionName, char *configPath, error_return *error);
 char *findFunctionPath(char *functionName, error_return *error);
 matrix *executeUserFunction(char *functionPath, char **functionArgs, vari *var, error_return *error);
+uint8_t checkFunctionName(char *functionName, char *directory);
 
 #endif //USER_FUNCTIONS
